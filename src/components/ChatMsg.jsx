425556badc182ebb591d6c28a629d16eb63bb9a1
Ishimwe Muhire Sadi <yanicksadi10@gmail.com>
2026-03-24 19:53:22 +0200

    updated the ChatMsg Component

--- src/components/ChatMsg.jsx
+++ src/components/ChatMsg.jsx
@@ -5,29 +5,20 @@ import user from '../assets/user.png'
 
 function ChatMsg({message, sender}){
 
-  // const message = props.message;
-  // const sender = props.sender;
-  // const {message,sender} = props;
 
-  // if(sender === 'robot'){
-  //   return (
-  //     <div className="flex mt-10">
-  //       <img src={chatbot}  className="w-12.5 ml-10 gap-2" />
-  //       {message}
-  //     </div>
-  //   );
-  // }
   return(
 
     <div className="flex mt-10">
-      {sender === 'robot' && (
-        <img src={chatbot} className="w-12.5 ml-10 gap-2"/>
+        {sender === 'robot' && (
+          <img src={chatbot} className="w-12.5 ml-10 gap-2"/>
+          )}
+        {message}
+        {sender === 'user' && (
+          <img src={user}  className="w-12.5 ml-10 gap-2" /> 
         )}
-      {message}
-      {sender === 'user' && (
-        <img src={user}  className="w-12.5 ml-10 gap-2" /> 
-      )}
+
     </div>
+    
   );
 }
 
--- src/components/DisplayMessage.jsx
+++ src/components/DisplayMessage.jsx
@@ -1,7 +1,14 @@
-import {useEffect,useState} from 'react'
+// import {useEffect,useState} from 'react'
 import ChatMsg from './ChatMsg'
+
+
+
+
+
+
 function DisplayMessage(){
-  const chatMessages = [{
+ 
+ const chatMessages = [{
     message: "hello chatbot",
     sender: "user",
     id: 'id1'
@@ -23,32 +30,40 @@ function DisplayMessage(){
   }
 ]
 
+function sendMessage(){
+ chatMessages.push({
+  message: 'test',
+  sender: 'user',
+  id: crypto.randomUUID()
+ });
+ console.log(chatMessages)
+}
+
  return(
-  <>
+  <div>
 
     <div>
       <input className="border flex-1 p-2 mt-10 ml-10" placeholder="send a message to Chatbot" 
         size="30"
       />
 
-      <button className="bg-green-600 border p-2 ml-2 rounded">
+      <button onClick={sendMessage} className="bg-green-600 border p-2 ml-2 rounded">
         Send
       </button>
+   
+          {chatMessages.map((chatMessage)=>{
+        return (
+          <ChatMsg 
+            message={chatMessage.message}
+            sender={chatMessage.sender}
+            key={chatMessage.id}
+          />
+        );
+      })}
     </div>
     
-    <div>
-         {chatMessages.map((chatMessage)=>{
-      return (
-        <ChatMsg 
-          message={chatMessage.message}
-          sender={chatMessage.sender}
-          key={chatMessage.id}
-        />
-      );
-    })}
-    </div>
     
-  </>
+  </div>
   );
 }
 
