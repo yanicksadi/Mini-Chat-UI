eab765c5de6a0a8a204a9dc36da23970a7fbdf52
Ishimwe Muhire Sadi <yanicksadi10@gmail.com>
2026-03-25 16:18:17 +0200

    Add Styling to the Messages

--- src/components/ChatMsg.jsx
+++ src/components/ChatMsg.jsx
@@ -3,17 +3,18 @@ import user from '../assets/user.png'
 
 function ChatMsg({ message, sender }) {
   return (
-    <div className="flex ml-16 mt-10 items-center">
-      {sender === 'robot' && (
-        <img src={chatbot} className="w-[30px] mr-10" />
-      )}
+      <div className={`flex items-center mb-2 ${sender === 'user' ? 'justify-end': 'justify-start'}`}>
 
-      <p>{message}</p>
+      {sender === 'robot' && (
+        <img src={chatbot} className="w-[30px] mr-2"/>
+      )}
+      <p className={`px-3 py-2 rounded-md max-w-xs ${sender ==='user' ? 'bg-green-600 text-white':'bg-gray-200 text-black'}`}>{message}</p>
 
       {sender === 'user' && (
-        <img src={user} className="w-[30px] ml-10" />
+        <img src={user} className="w-[30px] ml-2" />
       )}
-    </div>
+      </div>
+      
   );
 }
 
--- src/components/DisplayMessage.jsx
+++ src/components/DisplayMessage.jsx
@@ -40,31 +40,35 @@ function DisplayMessage({ chatMessages, setChatMessages }) {
   }
 
   return (
-    <div>
-      <input
-        className="border flex-1 p-2 ml-10"
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex w-full max-w-md gap-2 mb-6">
+         <input
+        className="border-[3px] p-2 rounded-md flex-grow"
         placeholder="send a message to Chatbot"
-        size="30"
         value={inputText}
         onChange={saveInputText}
       />
 
       <button
         onClick={sendMessage}
-        className="bg-green-600 border p-2 ml-2 rounded"
+        className="bg-green-600 py-2 px-4 rounded-md text-white cursor-pointer"
       >
         Send
       </button>
-
-      {chatMessages.map((chatMessage) => {
-        return (
-          <ChatMsg
-            key={chatMessage.id}
-            message={chatMessage.message}
-            sender={chatMessage.sender}
-          />
-        );
-      })}
+      </div>
+     
+      <div className="w-full max-w-md flex flex-col gap-3">
+        {chatMessages.map((chatMessage) => {
+          return (
+            <ChatMsg
+              key={chatMessage.id}
+              message={chatMessage.message}
+              sender={chatMessage.sender}
+            />
+          );
+        })}
+      </div>
+    
     </div>
   );
 }
